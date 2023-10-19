18042c6fd26b2ad526f369b0084569fe86eeaa45
Pradumm <kpradumm@gmail.com>
2023-10-19 12:04:40 +0530

    smj

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+ import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './componants/Navbar';
 import Shop from './componants/Shop';
+import SingleProduct from './componants/SingleProduct';
 // import { Home } from './componants/pages/Home';
 // import { About } from './componants/pages/About';
 // import { Services } from './componants/pages/Services';
@@ -10,11 +14,21 @@ import Shop from './componants/Shop';
 
 
 
+
+
 function App() {
   return (
     <>
-      <Navbar />
-      <Shop/>
+      <BrowserRouter>
+        <Routes>
+        {/* <Route path='/' element={<Shop />} /> */}
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/singleproduct' element={<SingleProduct />} />
+        </Routes>
+
+      </BrowserRouter>
+      {/* <Navbar /> */}
+      {/* <Shop/> */}
 
     </>
   );
